2494276ac92dcde4d34c37f1a849291e4d460fd1
Kevin Yamauchi <kevin.yamauchi@gmail.com>
2019-07-18 18:16:05 -0700

    added module directories

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='InSituToolkit',
+      version='0.0.1',
+      description='Tools for in situ transcriptomics analysis',
+      url='https://github.com/czbiohub/InSituToolkit',
+      author='Kevin Yamauchi',
+      author_email='kevin.yamauchi@czbiohub.org',
+      license='MIT',
+      packages=['InSituToolkit'],
+      zip_safe=False)
